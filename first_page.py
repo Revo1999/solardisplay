1b43b08b08c5332b3518164ac00d38c327d4ed91
Revo1999 <victor.rassers@gmail.com>
2024-07-04 11:52:45 +0200

    initial upload 3

--- first_page.py
+++ first_page.py
@@ -1,4 +1,18 @@
 import streamlit as st
 
-st.title("Hello, World!")
-st.write("This is your first Streamlit app.")
+st.title("Setup solar display")
+
+# Title of the box
+st.subheader("Initial Setup")
+
+# Text inputs for email, country, city
+email = st.text_input("Email")
+country = st.text_input("Country")
+city = st.text_input("City")
+
+# Button to submit the form
+if st.button("Enter"):
+    # Action to perform when the button is clicked
+    st.write("Email:", email)
+    st.write("Country:", country)
+    st.write("City:", city)
